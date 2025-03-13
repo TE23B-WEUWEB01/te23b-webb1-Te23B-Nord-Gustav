f17a8c01fcd0a8922964e3fa4b4dd8d8d010ac80
Gustav.Nordlander <Gustav.nordlander@elev.ga.ntig.se>
2025-03-13 11:07:45 +0100

    tränat js

---
+++ Kapitel-8/intro/script.js
@@ -0,0 +1,14 @@
+
+// var ålder = 17;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2
+// alert(tal1+"+"+tal2+"="+summa);
+
+var namn = "gurre";
+var efternamn = "burre";
+var helnamn = namn+" "+efternamn;
+alert(helnamn);
+
+var mening = namn+ " \när en fin mäniska hans efternamn är "+ efternamn;
+alert(mening);
